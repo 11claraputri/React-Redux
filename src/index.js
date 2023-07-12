958712cca4c1ff34ff3ddbf206a155116e4904ce
11claraputri <11claraputri@gmail.com>
2023-07-12 17:41:12 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider, createStoreHook } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers'
 
+const store = createStore(reducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
